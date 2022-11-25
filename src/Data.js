621eceb2882b5675e440f3a79879c5222965808b
unknown <ritochka.koval@yandex.ru>
2022-11-26 04:07:00 +0500

    cart page added

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,3 @@
-
-let shop = document.getElementById("shop")
-
 let shopItemsData = [{
     id: "123456",
     name:"Клетчатка сибирская Правильное Похудение 2.0 с пребиотиками",
@@ -37,71 +34,3 @@ let shopItemsData = [{
     desc:"Облегчает работу печени за счет своевременного выведения пищевых и бактериальных токсинов, канцерогенов из организма.",
     img:"images/img5.jpg",
     }]
-
-console.log(shop);
-
-let basket = [];
-
-let generateShop =()=>{
-    return (shop.innerHTML = shopItemsData.map((x)=>{
-        let {id,name,price,desc,img} = x;
-        
-        return `
-      <div id=product-id-${id} class="item">
-          <img width="220" src=${img} alt="">
-          <div class="details">
-            <h3>${name}</h3>
-            <p>${desc}</p>
-            <div class="price-quantity">
-              <h2>$ ${price} </h2>
-              <div class="buttons">
-                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                <div id=${id} class="quantity">0</div>
-                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-      }).join(""));
-};
-
-generateShop();
-
-let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem);
-
-  if (search === undefined) {
-    basket.push({
-      id: selectedItem,
-      item: 1,
-    });
-  } else {
-    search.item += 1;
-  }
-
-  // console.log(basket);
-  update(selectedItem);
-  
-};
-let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem);
-
-  if (search === undefined) return;
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-  update(selectedItem);
-  basket = basket.filter((x) => x.item !== 0);
-  // console.log(basket);
-
-};
-let update = (id) => {
-  let search = basket.find((x) => x.id === id);
-  // console.log(search.item);
-  document.getElementById(id).innerHTML = search.item;
-  
-};
